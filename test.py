ba239bee5e0dcedc475da7f4679144b0d39b2637
Rharif Anass <88338613+arharif@users.noreply.github.com>
2026-06-06 12:07:45 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,11 +1,33 @@
-PLUGINS = [
-    "netbox_topology_views",
-]
+services:
 
-PLUGINS_CONFIG = {
-    "netbox_topology_views": {
-        "static_image_directory": "netbox_topology_views/img",
-        "allow_coordinates_saving": True,
-        "always_save_coordinates": True,
-    }
-}
+  tls:
+    image: caddy:2-alpine
+    depends_on:
+      - netbox
+    volumes:
+      - ./net-netbox_2025.crt:/etc/ssl/private/cert.crt:ro,z
+      - ./net-netbox_2025.key:/etc/ssl/private/key.key:ro,z
+      - ./Caddyfile:/etc/caddy/Caddyfile:ro
+    ports:
+      - "80:80"
+      - "443:443"
+    restart: unless-stopped
+
+  netbox:
+    image: netbox:v4.4-3.4.1-topology
+    pull_policy: never
+    restart: unless-stopped
+
+  netbox-worker:
+    image: netbox:v4.4-3.4.1-topology
+    pull_policy: never
+    restart: unless-stopped
+
+  postgres:
+    restart: unless-stopped
+
+  redis:
+    restart: unless-stopped
+
+  redis-cache:
+    restart: unless-stopped
