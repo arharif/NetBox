fb3a79b39f651696d5cd60ee30a3ddf9919b5f3e
Rharif Anass <88338613+arharif@users.noreply.github.com>
2026-06-06 11:30:05 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,17 +1,11 @@
-FROM netboxcommunity/netbox:v4.4-3.4.1
+PLUGINS = [
+    "netbox_topology_views",
+]
 
-USER root
-
-COPY plugin_requirements.txt /opt/netbox/plugin_requirements.txt
-
-RUN /usr/local/bin/uv pip install -r /opt/netbox/plugin_requirements.txt
-
-RUN mkdir -p /opt/netbox/netbox/static/netbox_topology_views/img && \
-    chown -R unit:root /opt/netbox/netbox/static/netbox_topology_views
-
-RUN SECRET_KEY="dummyKeyWithMinimumLength-----------------------------" \
-    /opt/netbox/venv/bin/python /opt/netbox/netbox/manage.py collectstatic --no-input
-
-USER unit
-
-WORKDIR /opt/netbox/netbox
+PLUGINS_CONFIG = {
+    "netbox_topology_views": {
+        "static_image_directory": "netbox_topology_views/img",
+        "allow_coordinates_saving": True,
+        "always_save_coordinates": True,
+    }
+}
