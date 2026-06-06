e6eee110494c12048f2dc3f8aaf823f295f1373e
Rharif Anass <88338613+arharif@users.noreply.github.com>
2026-06-06 11:12:35 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 cat > Caddyfile <<'EOF'
-net-netbox.oecd.org net-netbox-a.oecd.org net-netbox-b.oecd.org dkinetbox.oecd.org 10.102.4.81 10.102.4.82 {
+lol {
     tls /etc/ssl/private/cert.crt /etc/ssl/private/key.key
 
     encode gzip
