e0d40c0d1cc6d41c884e38f642317fff1ed5432e
Rharif Anass <88338613+arharif@users.noreply.github.com>
2026-06-06 12:02:42 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,20 +1,11 @@
-FROM netboxcommunity/netbox:v4.4-3.4.1
+PLUGINS = [
+    "netbox_topology_views",
+]
 
-USER root
-
-COPY plugin_requirements.txt /opt/netbox/plugin_requirements.txt
-
-RUN HTTPS_PROXY=http://wsg-proxy.oecd.org:443 \
-    HTTP_PROXY=http://wsg-proxy.oecd.org:443 \
-    /usr/local/bin/uv pip install \
-    --allow-insecure-host pypi.org \
-    --allow-insecure-host files.pythonhosted.org \
-    --allow-insecure-host pypi.python.org \
-    -r /opt/netbox/plugin_requirements.txt
-
-RUN mkdir -p /opt/netbox/netbox/static/netbox_topology_views/img && \
-    chown -R unit:root /opt/netbox/netbox/static/netbox_topology_views
-
-USER unit
-
-WORKDIR /opt/netbox/netbox
+PLUGINS_CONFIG = {
+    "netbox_topology_views": {
+        "static_image_directory": "netbox_topology_views/img",
+        "allow_coordinates_saving": True,
+        "always_save_coordinates": True,
+    }
+}
